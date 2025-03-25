eaec669f7aaa8c67c84be5ca540c2eadc2d7c691
shrub <shrub719@gmail.com>
2025-03-25 19:07:36 +0000

    added basic educational curve using tinyec

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
 from tinyec.ec import SubGroup, Curve
+
+field = SubGroup(p=17, g=(15, 13), n=18, h=1)
+curve = Curve(a=0, b=7, field=field, name="p1707")
+
+print(curve)
+
+for k in range(25):
+    p = k * curve.g
+    print(f"{k}G = ({p.x}, {p.y})")
