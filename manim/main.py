7c636284fb452527cbe5b52ef1f5d10e856fd505
shrub <shrub719@gmail.com>
2025-03-27 20:53:29 +0000

    added curve animation

--- manim/main.py
+++ manim/main.py
@@ -1,43 +1,17 @@
 from manim import *
 
-class DefaultTemplate(Scene):
-    def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
-
-        square = Square()  # create a square
-        square.flip(RIGHT)  # flip horizontally
-        square.rotate(-3 * TAU / 8)  # rotate a certain amount
-
-        self.play(Create(square))  # animate the creation of the square
-        self.play(Transform(square, circle))  # interpolate the square into the circle
-        self.play(FadeOut(square))  # fade out animation
-
-
 class EC(Scene):
     def construct(self):
         graph = ImplicitFunction(
             lambda x, y: x**3 + 7 - y**2,
             color=BLUE
         )
-        self.add(NumberPlane(), graph)
+        plane = NumberPlane()
+        self.add(plane)
+        self.play(Create(graph))
 
-class ExampleFunctionGraph(Scene):
-    def construct(self):
-        cos_func = FunctionGraph(
-            lambda t: np.cos(t) + 0.5 * np.cos(7 * t) + (1 / 7) * np.cos(14 * t),
-            color=RED,
-        )
 
-        sin_func_1 = FunctionGraph(
-            lambda t: np.sin(t) + 0.5 * np.sin(7 * t) + (1 / 7) * np.sin(14 * t),
-            color=BLUE,
-        )
-
-        sin_func_2 = FunctionGraph(
-            lambda t: np.sin(t) + 0.5 * np.sin(7 * t) + (1 / 7) * np.sin(14 * t),
-            x_range=[-4, 4],
-            color=GREEN,
-        ).move_to([0, 1, 0])
-
-        self.add(cos_func, sin_func_1, sin_func_2)
+if __name__ == "__main__":
+    with tempconfig({"preview": True}):
+        scene = EC()
+        scene.render()
