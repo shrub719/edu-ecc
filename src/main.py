9179c91dc4bd2782cdf90f34fd24c1e99027dd8d
shrub719 <shrub719@gmail.com>
2025-03-27 14:17:00 +0000

    added manim test

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 from tinyec.ec import SubGroup, Curve
+from manim import *  # TODO: install manim, doesn't work on mb
+
+class EC(Scene):
+    def construct(self):
+        graph = ImplicitFunction(
+            lambda x, y: x ** 3 + 3 - y**2,
+            color=BLUE
+        )
+        self.add(NumberPlane(), graph)
+
 
 field = SubGroup(p=17, g=(15, 13), n=18, h=1)
 curve = Curve(a=0, b=7, field=field, name="p1707")
