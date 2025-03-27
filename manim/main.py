0e34234ecb3abdfcdbc2945de335d7de285eab6d
shrub <shrub719@gmail.com>
2025-03-27 19:49:46 +0000

    added more examples

--- manim/main.py
+++ manim/main.py
@@ -1,13 +1,43 @@
 from manim import *
 
-class AxesTemplate(Scene):
+class DefaultTemplate(Scene):
     def construct(self):
-        graph = Axes(
-            x_range=[-1,10,1],
-            y_range=[-1,10,1],
-            x_length=9,
-            y_length=6,
-            axis_config={"include_tip":False}
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.flip(RIGHT)  # flip horizontally
+        square.rotate(-3 * TAU / 8)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+
+
+class EC(Scene):
+    def construct(self):
+        graph = ImplicitFunction(
+            lambda x, y: x**3 + 7 - y**2,
+            color=BLUE
         )
-        labels = graph.get_axis_labels()
-        self.add(graph, labels)
+        self.add(NumberPlane(), graph)
+
+class ExampleFunctionGraph(Scene):
+    def construct(self):
+        cos_func = FunctionGraph(
+            lambda t: np.cos(t) + 0.5 * np.cos(7 * t) + (1 / 7) * np.cos(14 * t),
+            color=RED,
+        )
+
+        sin_func_1 = FunctionGraph(
+            lambda t: np.sin(t) + 0.5 * np.sin(7 * t) + (1 / 7) * np.sin(14 * t),
+            color=BLUE,
+        )
+
+        sin_func_2 = FunctionGraph(
+            lambda t: np.sin(t) + 0.5 * np.sin(7 * t) + (1 / 7) * np.sin(14 * t),
+            x_range=[-4, 4],
+            color=GREEN,
+        ).move_to([0, 1, 0])
+
+        self.add(cos_func, sin_func_1, sin_func_2)
