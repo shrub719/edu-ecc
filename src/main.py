c3ed3182a14eaf496cba4fbe96f0472f6de76509
shrub719 <shrub719@gmail.com>
2025-03-28 09:19:06 +0000

    plotted ec with pyplot

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
 from tinyec.ec import SubGroup, Curve
+import matplotlib.pyplot as plt
+import numpy as np
 
 field = SubGroup(p=17, g=(15, 13), n=18, h=1)
 curve = Curve(a=0, b=7, field=field, name="p1707")
 
 print(curve)
 
-for k in range(25):
+x = []
+y = []
+p = curve.g
+k = 1
+while p.x:
     p = k * curve.g
     print(f"{k}G = ({p.x}, {p.y})")
+    x.append(p.x)
+    y.append(p.y)
+    k += 1
+
+plt.plot(x, y, "o")
+plt.show()
