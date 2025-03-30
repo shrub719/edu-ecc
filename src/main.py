87066840e8832e3247a672ab05bd1a7007437b80
shrub <shrub719@gmail.com>
2025-03-30 13:51:59 +0100

    implemented modular division for point addition

--- src/main.py
+++ src/main.py
@@ -1,30 +1,42 @@
-# from tinyec.ec import SubGroup, Curve
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
+import math
 
-# field = SubGroup(p=17, g=(15, 13), n=18, h=1)
-# curve = Curve(a=0, b=7, field=field, name="p1707")
+
+def coefficient(x):
+    if x < 0: 
+        return f"- {x}"
+    else: 
+        return f"+ {x}"
+
+def inverse(x, m):
+    g = math.gcd(x, m) 
+    if (g != 1):
+        raise ValueError("Inverse does not exist.")
+    else:  
+        # modulo inverse is b^(m-2) mod m - ??!??!?!
+        return pow(x, m - 2, m)
+
+def divide(a, b, m):
+    a = a % m
+    inv = inverse(b, m)
+    return (inv * a) % m
+
 
 class Curve:
     def __init__(self, a, b, p):
         self.a = a
         self.b = b
         self.p = p
-
-    @staticmethod
-    def _coefficient(x):
-        if x < 0: 
-            return f"- {x}"
-        else: 
-            return f"+ {x}"
     
     def __repr__(self):
-        a = self._coefficient(self.a)
-        b = self._coefficient(self.b)
+        a = coefficient(self.a)
+        b = coefficient(self.b)
 
         return f"y^2 = x^3 {a}x {b}"
     
     def point(self, x, y):
         return Point(self, x, y)
+        
     
 class Point:
     def __init__(self, curve, x, y):
@@ -45,8 +57,10 @@ class Point:
 
         x1, y1, x2, y2 = self.x, self.y, other.x, other.y
 
-        x3 = ( (y2 - y1) // (x2 - x1) )^2 - x1 - x2
-        y3 = ( (y2 - y1) // (x2 - x1) ) * (x1 - x3) - y1
+        x3 = (self.div((y2 - y1), (x2 - x1)))**2 - x1 - x2
+        y3 = self.div((y2 - y1), (x2 - x1)) * (x1 - x3) - y1
+        x3 = x3 % self.curve.p
+        y3 = y3 % self.curve.p
         return self.curve.point(x3, y3)
     
     def __mul__(self, other):
@@ -54,13 +68,16 @@ class Point:
 
     def __repr__(self):
         return f"({self.x}, {self.y})"
+    
+    def div(self, a, b):
+        return divide(a, b, self.curve.p)
 
 
 curve = Curve(4, 20, 29)
 print(curve)
 p1 = curve.point(5, 22)
 p2 = curve.point(16, 27)
-print(p1, "+", p2, "=", p1 + p2)  # FIX well. it's wrong
+print(p1, "+", p2, "=", p1 + p2)
 
 # x = []
 # y = []
