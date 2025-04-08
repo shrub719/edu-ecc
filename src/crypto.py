cf22baa720ab27971f7aefaa7512a2437f10adc5
shrub719 <shrub719@gmail.com>
2025-04-08 17:25:38 +0100

    added crypto library

---
+++ src/crypto.py
@@ -0,0 +1,18 @@
+from ec import *
+from random import randint
+
+class Client:
+    def __init__(self, curve: Curve, generator: tuple, order: int):
+        self.curve = curve
+        if not is_coordinate(generator): raise ValueError("generator must be a valid coordinate")
+        self.generator = self.curve.point(generator[0], generator[1])
+        self.generate_keys(order)
+
+    def generate_keys(self, order: int):
+        # FIX: is the range correct?
+        self.__private = randint(2, order-1)  # NOT cryptographically secure
+        self.public = self.generator * self.__private
+    
+    def get_shared_key(self, other: Curve):
+        key = other.public * self.__private
+        return key
