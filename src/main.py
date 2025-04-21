6ab6165165ba5251dba9a98a11157f33e54abf82
shrub <shrub719@gmail.com>
2025-04-21 11:14:10 +0100

    added big demo

--- src/main.py
+++ src/main.py
@@ -68,5 +68,19 @@ def ecdh_mesh_demo():
     plt.show()
 
 
-ecdh_demo()
-ecdh_mesh_demo()
+def big_demo():
+    x, y = [], []
+    big = Curve(0, 7, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F)
+    g = big.point(0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798, 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8)
+    k = 1
+    while k <= 1000:
+        g = k * g
+        x.append(g.x)
+        y.append(g.y)
+        k += 1
+
+    plt.plot(x, y, "o")
+    plt.title(big)
+    plt.show()
+
+big_demo()
